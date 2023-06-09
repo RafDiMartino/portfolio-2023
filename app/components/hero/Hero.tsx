accd435ab9279476cf466d936442967fe12fa625
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-06-09 12:48:41 +0100

    testing gsap animations

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -1,13 +1,46 @@
-import React from 'react'
+"use client"
+import React, {useRef, useEffect, useState} from 'react'
 import classes from  "./Hero.module.css"
 import Link from 'next/link'
+import gsap from 'gsap'
+// import { gsap, Power3 } from 'gsap'
+
 
 export const Hero = () => {
+
+    let fullname = React.useRef<HTMLHeadingElement>(null)
+    let standfirst = React.useRef<HTMLParagraphElement>(null)
+
+
+
+    useEffect(() => {
+        gsap.from( 
+            fullname.current, {
+            duration: 2,
+            // y: -500,
+            autoAlpha: 0,
+            ease: "none",
+            delay: 1
+        })
+    },[fullname])
+
+    useEffect(() => {
+        gsap.from( 
+            standfirst.current, {
+            duration: 2,
+            // y: -500,
+            autoAlpha: 0,
+            ease: "back.out(0.5)",
+            delay: 2
+        })
+    },[standfirst])
+    
+
     return (
         <section className={classes.heroSection}>
             {/* <div className={classes.heroSectionWrapper}> */}
-                <h1 className={`${classes.name} ${classes.heroSectionWrapper}`}>RAF DI MARTINO</h1>
-                <p className={`${classes.about}`}>Web Developer with an Art & Design background</p>
+                <h1 ref={fullname} className={`${classes.name}`}>RAF DI MARTINO</h1>
+                <p ref={standfirst} className={`${classes.about}`}>Web Developer with an Art & Design background</p>
             {/* </div> */}
         </section>
     )
