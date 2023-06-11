2d2d7f032becf08eea31ffdca0bdcc3d031fb5db
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-06-11 19:42:17 +0100

    testing hero component animation

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -1,8 +1,10 @@
 "use client"
-import React, {useRef, useEffect, useState} from 'react'
+import React, {useRef,useLayoutEffect, useEffect, useState} from 'react'
 import classes from  "./Hero.module.css"
 import Link from 'next/link'
 import gsap from 'gsap'
+// import { TextPlugin } from 'gsap/dist/TextPlugin';
+// gsap.registerPlugin(TextPlugin);
 // import { gsap, Power3 } from 'gsap'
 
 
@@ -14,34 +16,63 @@ export const Hero = () => {
 
 
     useEffect(() => {
-        gsap.from( 
-            fullname.current, {
-            duration: 2,
-            // y: -500,
+
+        let nameReveal = gsap.timeline()
+        nameReveal.fromTo(".test", 
+        {
             autoAlpha: 0,
-            ease: "none",
-            delay: 1
-        })
-    },[fullname])
+        },
+        {
+            autoAlpha: 1,
+            duration: 2,
+            delay: 1,
+            stagger: {
+                each: 0.04,
+            }
+        }
+        )
+
+        // gsap.fromTo( 
+        //     fullname.current,
+        //     {
+        //         autoAlpha: 0,
+        //     },
+        //     {
+        //         autoAlpha: 1,
+        //         duration: 2,
+        //         ease: "none",
+        //         delay: .5
+        //     }
+        // )
+    },[])
 
     useEffect(() => {
-        gsap.from( 
-            standfirst.current, {
-            duration: 2,
-            // y: -500,
-            autoAlpha: 0,
-            ease: "back.out(0.5)",
-            delay: 2
-        })
-    },[standfirst])
+        gsap.fromTo( 
+            standfirst.current,             
+            {
+                autoAlpha: 0,
+            },
+            {
+                autoAlpha: 1,
+                duration: 1,
+                ease: "none",
+                delay: 2
+            }
+        )
+    },[])
     
 
     return (
-        <section className={classes.heroSection}>
-            {/* <div className={classes.heroSectionWrapper}> */}
-                <h1 ref={fullname} className={`${classes.name}`}>RAF DI MARTINO</h1>
+        <section className={`${classes.heroSection}`}>
+            <h1 className={classes.nameWrapper}>
+                {
+                    "RAF DI MARTINO".split("").map((word, i) => {
+                        return word === " " ? <span ref={fullname} key={i} className={`${classes.name} test`} >&nbsp;</span> : <span ref={fullname} key={i} className={`${classes.name} test`}>{word}</span>
+                    })
+                }
+                </h1>
                 <p ref={standfirst} className={`${classes.about}`}>Web Developer with an Art & Design background</p>
-            {/* </div> */}
+            
         </section>
     )
 }
