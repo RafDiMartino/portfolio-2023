85675dd95ff0c7f7cc3426f49b92234bc7bff860
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-06-17 16:59:25 +0100

    testing scroll trigger animation

--- app/components/skills/Skill.tsx
+++ app/components/skills/Skill.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Image from 'next/image'
 import classes from "./Skills.module.css"
-
+ 
 interface skillData {
     src: string;
     alt: string;
@@ -11,7 +11,7 @@ interface skillData {
 export const Skill = ({ src, alt, test }: skillData) => {
 
     return (
-        <div>
+        <div className="skillAnimation">
             <div className={classes.tooltip}>{test}</div>
             <Image width={0} height={0} src={src} alt={alt} priority={true} />
         </div>
--- app/components/skills/Skills.tsx
+++ app/components/skills/Skills.tsx
@@ -1,17 +1,39 @@
-import React from 'react'
+"use client"
+import React, { useRef, useEffect} from 'react'
 import classes from "./Skills.module.css"
-import  { Skill } from "./Skill"
+import { Skill } from "./Skill"
 import skill from "../../data/skills.json"
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
 
 export const Skills = () => {
 
+  useEffect(() => {
+    gsap.fromTo(
+        ".skillsSectionAimation",
+        {
+            autoAlpha: 0,
+        },
+        {
+            autoAlpha: 1,
+            duration: 1,
+            ease: "power2.in",
+            delay: 1,
+            scrollTrigger: {
+              trigger: ".skillsSectionAimation"
+            }
+        }
+    )
+}, [])
+
   return (
-    <section className={classes.skillsContainer}>
-        <h2>SKILLS</h2>
-        <p>Technologies I have used in my web development career</p>
-        <div className={classes.skillWrapper}>
-        {skill.map((skill, i) => <Skill  src={skill.src} alt={skill.alt} key={i} test={skill.test}/>)}
-        </div>
+    <section className={`${classes.skillsContainer} skillsSectionAimation`}>
+      <h2>SKILLS</h2>
+      <p >Technologies I have used in my web development career</p>
+      <div className={classes.skillWrapper}>
+        {skill.map((skill, i) => <Skill src={skill.src} alt={skill.alt} key={i} test={skill.test} />)}
+      </div>
     </section>
   )
 }
